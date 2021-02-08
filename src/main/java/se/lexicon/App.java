af88def285b7b9ce80977fd6216d6ee4479919bc
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-02-08 15:38:16 +0100

    Buffer Reader

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,19 +1,27 @@
 package se.lexicon;
 
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class App
 {
     public static void main( String[] args ) {
+
+        //Call for method to write content to text file.
         List<String> content= Arrays.asList("Hello", "Sebastian");
         File url= new File("/Users/sebastianbocaciu/Documents/BufferWriter.txt");
         write(url, content);
+
+
+        //Call for method to print out content in text file.
+        File url2 = new File ("/Users/sebastianbocaciu/Documents/BufferWriter.txt");
+        for (String string : read(url2)){
+            System.out.println(string);
+        }
+
 
 
     }
@@ -29,6 +37,19 @@ public class App
             e.printStackTrace();
         }
         return text;
+    }
+
+    public static List<String> read(File source) {
+        List<String> strings = new ArrayList<>();
+        try (BufferedReader reader= new BufferedReader(new FileReader(source))) {
+            String line;
+            while((line= reader.readLine()) != null) {
+                strings.add(line);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return strings;
     }
 
 }
